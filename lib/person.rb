87ae8c8389057719362dc42ede8686460159003d
thexyman <makdaa1@gmail.com>
2018-08-29 16:35:58 +0100

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  #writer method
+  def name=(person_name)
+    @name = person_name
+  end
+
+  #reader method
+  def name
+    @name
+  end
+
+  #writer method
+  def job=(job)
+    @job = job
+  end
+
+  #reader method
+  def job
+    @job
+  end
+
+end
